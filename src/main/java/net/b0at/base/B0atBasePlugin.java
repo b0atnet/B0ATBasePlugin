f7392993d1cd899b3739aabfd56400972ebd25ce
Jordin <jordinjm@gmail.com>
2017-08-05 18:07:57 -0300

    Initial commit.

---
+++ src/main/java/net/b0at/base/B0atBasePlugin.java
@@ -0,0 +1,11 @@
+package net.b0at.base;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Jordin on 8/5/2017.
+ * Jordin is still best hacker.
+ */
+public class B0atBasePlugin extends JavaPlugin {
+    // TODO: have a messagePlayer that reads a prefix from a config.
+}
